ab953f8c1d4289a9e3eed6a3189885975e63dca8
Parth Borle <parthborle0@gmail.com>
2024-03-27 11:43:44 +0530

    files added

---
+++ bubble sort.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int swap(int *a, int *b) { // swaping function if elements are in wrong order
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int printArray(int arr[], int size) {  // printing array function
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int bubbleSort(int arr[], int size) {		// function of bubble sort
+    int MAX_SIZE = size;
+    for (int i = 0; i < MAX_SIZE - 1; i++) {
+        int lb = 0;
+        int ub = MAX_SIZE - i - 1;
+        for (int j = lb; j < ub; j++) {
+            // Compare  elements
+            if (arr[j] > arr[j + 1]) {
+                // Swap them if they are in the wrong order
+                swap(&arr[j], &arr[j + 1]);
+
+                // Print the current state of the array
+                printf("Step %d: ", i * (MAX_SIZE - 1) + j + 1);
+                printArray(arr, size);
+                
+            }
+        }
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter the number of elements: ");
+    scanf("%d", &size);
+
+    int arr[100]; // Assuming maximum size of array is 100
+
+    printf("Enter the elements: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original Array: ");			//calling the functions
+    printArray(arr, size);              
+
+    bubbleSort(arr, size);
+
+    printf("Sorted Array: ");
+    printArray(arr, size);
+
+    return 0;
+}
+
