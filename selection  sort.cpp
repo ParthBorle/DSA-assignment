5313b57f0bb07babf00bff75fb177b68f62d1842
ParthBorle <142043084+ParthBorle@users.noreply.github.com>
2024-04-05 17:36:15 +0530

    Update selection  sort.cpp

--- selection  sort.cpp
+++ selection  sort.cpp
@@ -1,41 +1,58 @@
 #include <stdio.h>
 
+// Function to perform selection sort on an array
 int selectionSort(int arr[], int max) {
     int i, loc, small, tmp;
     
+    // iterate through the array
     for (i = 0; i < max - 1; i++) {
-        small = i;
+        small = i; // Assume the current index has the smallest value
+        
+        // Find the index of the smallest element in the unsorted part of the array
         for (loc = i + 1; loc < max; loc++) {
             if (arr[loc] < arr[small]) {
-                small = loc;
+                small = loc; // Update the index of the smallest element
             }
         }
+        
+        // Swap the smallest element with the current element 
         tmp = arr[small];
         arr[small] = arr[i];
         arr[i] = tmp;
+        
+        // Print the current step of the sorting process
+        printf("\nStep %d: ", i + 1);
+        for (loc = 0; loc < max; loc++) {
+            printf("%d ", arr[loc]); 
+        }
+        printf("\n");
     }
 }
 
 int main() {
     int max;
-    printf("Enter no of elements: ");
+    printf("Enter the number of elements: ");
     scanf("%d", &max);
     
     int arr[max];
     int i;
     
+    // Input the elements of the array from the user
     for (i = 0; i < max; i++) {
         printf("Enter element %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
     
+    // Display the original array
     printf("Original array: ");
     for (i = 0; i < max; i++) {
         printf("%d ", arr[i]);
     }
     
+    // Calling the selectionSort function 
     selectionSort(arr, max);
     
+    // Display the sorted array
     printf("\nSorted array: ");
     for (i = 0; i < max; i++) {
         printf("%d ", arr[i]);
@@ -43,4 +60,3 @@ int main() {
     
     return 0;
 }
-
