60fa192b30edbe7f1b07e27ac8ea9cdcf22e1e07
ParthBorle <142043084+ParthBorle@users.noreply.github.com>
2024-04-26 13:38:52 +0530

    Update linear queue function.cpp

--- linear queue function.cpp
+++ linear queue function.cpp
@@ -1,54 +1,5 @@
 #include<stdio.h>
 
-int insertion(int Q[], int val, int front, int rear, int MAX);
-int deletion(int Q[], int front, int rear);
-int display(int Q[], int front, int rear);
-
-int main() {
-    int MAX;
-    printf("Enter the length of the queue: ");
-    scanf("%d", &MAX);
-
-    int Q[MAX], front = -1, rear = -1;
-    int val, option;
-   
-    do {
-        printf("\n *****MAIN MENU*****");
-        printf("\n 1. INSERTION");
-        printf("\n 2. DELETION");
-        printf("\n 3. DISPLAY");
-        printf("\n 4. EXIT");
-        printf("\n Enter your option: ");
-        scanf("%d", option);
-
-        switch(option) {
-            case 1:
-                printf("Enter the element: ");
-                scanf("%d", &val);
-                insertion(Q, val, front, rear, MAX);
-                break;
-
-            case 2:
-                deletion(Q, front, rear);
-                break;
-
-            case 3:
-                display(Q, front, rear);
-                break;
-
-            case 4:
-                printf("GOOD BYE\n");
-                break;
-
-            default:
-                printf("Invalid option\n");
-                break;
-        }
-    } while(option != 4);
-
-    return 0;
-}
-
 int insertion(int Q[], int val, int front, int rear, int MAX) {
     if(rear == MAX - 1) {
         printf("Overflow\n");
@@ -95,4 +46,51 @@ int display(int Q[], int front, int rear) {
     }
     return 0;
 }
+
+
+int main() {
+    int MAX;
+    printf("Enter the length of the queue: ");
+    scanf("%d", &MAX);
+
+    int Q[MAX], front = -1, rear = -1;
+    int val, option;
+   
+    do {
+        printf("\n *****MAIN MENU*****");
+        printf("\n 1. INSERTION");
+        printf("\n 2. DELETION");
+        printf("\n 3. DISPLAY");
+        printf("\n 4. EXIT");
+        printf("\n Enter your option: ");
+        scanf("%d", option);
+
+        switch(option) {
+            case 1:
+                printf("Enter the element: ");
+                scanf("%d", &val);
+                insertion(Q, val, front, rear, MAX);
+                break;
+
+            case 2:
+                deletion(Q, front, rear);
+                break;
+
+            case 3:
+                display(Q, front, rear);
+                break;
+
+            case 4:
+                printf("GOOD BYE\n");
+                break;
+
+            default:
+                printf("Invalid option\n");
+                break;
+        }
+    } while(option != 4);
+
+    return 0;
+}
+
 
